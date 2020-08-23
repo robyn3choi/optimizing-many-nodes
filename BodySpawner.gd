2166499888b5d11e12095b0c902a368d01c810b7
Robyn Choi <robyn3choi@gmail.com>
2020-08-22 20:32:16 -0700

    multithreading example

---
+++ BodySpawner.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var body_scene = preload("res://Body.tscn")
+var rng = RandomNumberGenerator.new()
+var bodies = []
+var threads = []
+var num_threads = 8
+
+
+func _ready() -> void:
+	rng.randomize()
+	
+	for i in num_threads:
+		var thread = Thread.new()
+		threads.push_back(thread)
+	
+	for i in 1000:
+		var body = body_scene.instance()
+		var screen_size = get_viewport().get_visible_rect().size
+		var x = rng.randi_range(0, screen_size.x)
+		var y = rng.randi_range(0, screen_size.y)
+		body.position = Vector2(x, y)
+		body.left = i % 2 == 0
+		add_child(body)
+		bodies.push_back(body)
+
+
+func _process(delta):
+	for i in num_threads:
+		threads[i].start(self, "move_bodies", {"delta": delta, "index": i})
+	
+	for i in num_threads:
+		threads[i].wait_to_finish()
+
+
+func move_bodies(args):
+	var start = bodies.size() / num_threads * args.index
+	var end = start + bodies.size() / num_threads
+	if args.index == threads.size() - 1:
+		end += bodies.size() % num_threads
+	for i in range(start, end):
+		bodies[i].move(args.delta)
+
+
+#func _process(delta: float) -> void:
+#	for b in bodies:
+#		b.move(delta)
---
+++ TimeTo60Fps.gd
@@ -0,0 +1,9 @@
+extends Label
+
+var has_printed = false
+
+
+func _process(delta: float) -> void:
+	if Engine.get_frames_per_second() >= 60 && !has_printed:
+		text = String(OS.get_ticks_msec())
+		has_printed = true
